059ecb439d7a3a4b3ff989f0a8cd491ce82b7adb
John R Iversen <john@johniversen.org>
2019-01-12 16:56:43 -0800

    experimental fix for ultrasound module lockup problem

--- ModuleTests/testUltrasound/testUltrasound.ino
+++ ModuleTests/testUltrasound/testUltrasound.ino
@@ -5,10 +5,15 @@
 #define ECHO 10
 #define BUZZER 5
 // defines variables
-long duration;
-int distance;
+unsigned long duration;
+float distance;
 
-#define LONGEST_DISTANCE 200 // 200 cm = 2 meters
+//JRI Jan '19: My module 'locks up' in a mode when it emits very long echo pulses. It seems to do so when
+// unsure of the reflection, and does not have the built-in cutoff of 38ms advertised in the specs, but instead
+// gives a >100ms pulse. So, use a  long cutoff here and instead detect the problem below and power-cycle the 
+// ultrasonic module. This allows it to get one valid sample after every reset, at least. Otherwise, it would
+// simply emit a long series of '0' timeout readings.
+#define LONGEST_DISTANCE 3000 // 200 cm = 2 meters
 float farTime =  LONGEST_DISTANCE*2/0.034;
 
 void setup() {
@@ -26,15 +31,25 @@ void loop() {
   delayMicroseconds(2);
 
   // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(TRIGGER, HIGH);
   digitalWrite(BUZZER,HIGH);
+  digitalWrite(TRIGGER, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER, LOW);
   digitalWrite(BUZZER,LOW);
 
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(ECHO, HIGH, farTime);
-  Serial.println(counter++);
+  Serial.println(duration);
+
+//detect hung echo and reset by powercycle
+  if (duration > 50000) {
+    //Serial.println("RESET!");
+    digitalWrite(VCC, LOW);
+    delay(100);
+    digitalWrite(VCC, HIGH);
+    delay(100);
+    return;
+  }
   
 
   // Calculating the distance
@@ -44,4 +59,5 @@ void loop() {
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
+  delay(200);
 }
